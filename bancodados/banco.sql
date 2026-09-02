3027ca56b28995946b728ef276e629e9ca629125
JoaoP-Mendes <asstecmendes@gmal.com>
2026-09-01 22:08:08 -0400

    Definição da estrutura do projeto, criação conexao com banco e README

---
+++ bancodados/banco.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS analise_jogos;
+USE analise_jogos;
+
+CREATE TABLE IF NOT EXISTS jogos (
+    appid INT PRIMARY KEY, 
+    nome VARCHAR (255) NOT NULL,
+    tipo VARCHAR (255) NOT NULL DEFAULT 'game',
+    desenvolvedora VARCHAR (255) NOT NULL,
+    publicadora VARCHAR(255) NOT NULL,
+    genero VARCHAR(255) NOT NULL, 
+    gratuito ENUM('S', 'N') NOT NULL DEFAULT 's'
+) DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE IF NOT EXISTS historico(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    appid INT NOT NULL,
+    data_analise DATE NOT NULL,
+    preco FLOAT(5, 2) NOT NULL DEFAULT 0.00,
+    CONSTRAINT appid_jogo FOREIGN KEY (appid) REFERENCES jogos(appid)
+) DEFAULT CHARSET = utf8mb4;
